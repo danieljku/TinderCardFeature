638d91be09e08e66efb45abcfd047f1d16090e5f
DanielKu <danieljku@gmail.com>
2017-08-03 09:02:03 -0700

    Initial commit

---
+++ CardsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CardsViewController.swift
+//  TinderCardFeature
+//
+//  Created by Daniel Ku on 8/3/17.
+//  Copyright © 2017 djku. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderCardFeature
+//
+//  Created by Daniel Ku on 8/3/17.
+//  Copyright © 2017 djku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
