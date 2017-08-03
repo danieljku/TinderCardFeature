825bccd76c4321dfa2ad56ccba069a2b20ee8955
DanielKu <danieljku@gmail.com>
2017-08-03 14:03:49 -0700

    All features work

--- CardsViewController.swift
+++ CardsViewController.swift
@@ -10,15 +10,54 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    var cardInitialCenter: CGPoint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func didDragCard(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            cardImageView.transform = CGAffineTransform(rotationAngle: CGFloat(0))
+            cardInitialCenter = cardImageView.center
+            
+        } else if sender.state == .changed {
+            let rotation = (translation.x > 0) ? CGFloat(0.018) : CGFloat(-0.018)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            if translation.x > 100 {
+                UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options:[] , animations: { () -> Void in
+                    self.cardImageView.center = CGPoint(x: CGFloat(1000) , y: self.cardImageView.center.y)
+                }, completion: nil)
+            } else if translation.x < -100 {
+                UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options:[] , animations: { () -> Void in
+                    self.cardImageView.center = CGPoint(x: CGFloat(-1000) , y: self.cardImageView.center.y)
+                }, completion: nil)
+            }
+        
+            
+            
+            if location.y < cardImageView.center.y {
+                cardImageView.transform = cardImageView.transform.rotated(by: rotation)
+            } else {
+                cardImageView.transform = cardImageView.transform.rotated(by: -1 * rotation)
+            }
+            
+            cardImageView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y + translation.y)
+            
+        } else if sender.state == .ended {
+            cardImageView.center = cardInitialCenter
+            cardImageView.transform = CGAffineTransform.identity
+        }
+        
+        
     }
+
+
 
 }
